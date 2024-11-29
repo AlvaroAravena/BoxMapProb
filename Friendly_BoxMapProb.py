7f8e6340113dd8993410f3751b167cd1eb596905
aravenaponce <alvaro.aravena@uca.fr>
2024-11-29 14:09:54 +0100

    Printing options

--- Friendly_BoxMapProb.py
+++ Friendly_BoxMapProb.py
@@ -2000,6 +2000,7 @@ class MainFrame:
 			self.but_saveres.configure( state = 'disabled' )
 
 if __name__ == '__main__':
+	np.set_printoptions( legacy =' 1.25 ')
 	root = Tk()
 	my_gui = MainFrame( root )
 	root.after( 100 )
